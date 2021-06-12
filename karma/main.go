24b62adb2bc529282059c8c12407b6057586289e
Stanley <stanleygarbo@gmail.com>
2021-06-12 15:14:00 +0800

    change localhost to karma_redis_1

--- karma/main.go
+++ karma/main.go
@@ -49,7 +49,7 @@ func main(){
 		MaxIdle:     10,
 		IdleTimeout: 240 * time.Second,
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "localhost:6379")
+			return redis.Dial("tcp", "karma_redis_1:6379")
 		},
 	}
 
